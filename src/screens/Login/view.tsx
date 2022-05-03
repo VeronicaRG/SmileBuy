6544734add3c5d04b6f477ee23acd2bb6b6af015
Veronica RG <vero.rg0416@gmail.com>
2022-05-02 22:47:52 -0300

    feat: setup svg

--- src/screens/Login/view.tsx
+++ src/screens/Login/view.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Logo from '@src/assets/svgs/logo.svg';
+
 import { Container } from './styles';
 
 const LoginView: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Logo color="#fff" width={106} height={127} />
+    </Container>
+  );
 };
 
 export default LoginView;
